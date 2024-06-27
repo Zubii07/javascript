e35af55521817ed9a8116bb8bc2a8f50e2932bb5
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-06-27 10:06:05 +0500

    Add files via upload

---
+++ javascript/scope.js
@@ -0,0 +1,65 @@
+/* 
+   Scope determines where variables are accessible and how long they exist in a program.
+   Global scope in JavaScript is where a variable is available throughout the entire program.
+   Local scope refers to the limited range where a variable is accessible, such as within a function.
+   The "let" keyword in JavaScript helps control variable scope, preventing unintended consequences in code.
+*/
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Ali"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Ali"
+    if (username === "Ali") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){   // first syntax 
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){   // second syntax: store function in a variable 
+    return num + 2 
+}
