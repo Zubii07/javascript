adf89b9841ec29761187cf12065e4a4de29910d9
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-08-07 13:54:22 +0500

    Update script.js

--- javascript/Call Back Hell/script.js
+++ javascript/Call Back Hell/script.js
@@ -1,8 +1,13 @@
 
 // Function that is passed on as argument to another function is called callback function.
 
-//Event listeners consume a lot of memory which can potentially slow down the website 
-//therefore it is good practice to remove if it is not used.
+/* 1 - Callback hell
+When a function is passed as an argument to another function, it becomes a callback function. This process continues and there are many callbacks inside another's Callback function.
+This grows the code horizontally instead of vertically. That mechanism is known as callback hell. 
+
+2 - Inversion of control
+The callback function is passed to another callback, this way we lose the control of our code. We don't know what is happening behind the scene and the program becomes very difficult to maintain. 
+That process is called inversion of control.*/
 
 
 h1 = document.querySelector("h1");
